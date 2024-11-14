caeeb86c103270a2d9b09471c6d60d73863fcdc1
lujainjumaa <lujainjumaa2@gmail.com>
2024-11-14 23:49:43 +0300

    add getFirstRowValues ,getcell and removeInvisibleCharacters function in ExelControlller

--- src/main/java/org/example/ExelController.java
+++ src/main/java/org/example/ExelController.java
@@ -7,6 +7,8 @@ import org.apache.poi.ss.usermodel.Workbook;
 import org.apache.poi.xssf.usermodel.XSSFWorkbook;
 
 import java.io.*;
+import java.util.ArrayList;
+import java.util.List;
 
 public class ExelController {
 
@@ -25,10 +27,36 @@ public class ExelController {
         }
         return null;
     }
+
     public void test(String filepath) throws FileNotFoundException {
-        open(filepath);
         Row row = open(filepath).getRow(0);
         Cell cell = row.getCell(0);
         System.out.println(cell);
+    }
+
+    public List<String> getFirstRowValues(String filepath) throws FileNotFoundException {
+        List<String> rowValues = new ArrayList<>();
+        Row row = open(filepath).getRow(0);
+        for (Cell cell : row) {
+            switch (cell.getCellType()) {
+                case STRING:
+                    rowValues.add(cell.getStringCellValue());
+                    break;
+                case NUMERIC:
+                    rowValues.add(String.valueOf(cell.getNumericCellValue()));
+                    break;
+                default:
+                    rowValues.add("");
+                    break;
+            }
+        }
+        return rowValues;
+    }
+    public Cell getcell(int rowIndex ,int columnIndex,String filepath) throws FileNotFoundException {
+        Row row = open(filepath).getRow(rowIndex);
+        return row.getCell(columnIndex);
+    }
+    public String removeInvisibleCharacters(String input) {
+        return input.replaceAll("[\\p{C}\\p{Z}\\u00A0\\u200E\\u200F\\u202A\\u202B\\u202C\\u202D\\u202E\\uFEFF]", "").trim();
     }
 }
--- src/main/java/org/example/Main.java
+++ src/main/java/org/example/Main.java
@@ -1,6 +1,7 @@
 package org.example;
 
 import java.io.FileNotFoundException;
+import java.util.List;
 
 public class Main {
     public static void main(String[] args) throws FileNotFoundException {
@@ -8,5 +9,11 @@ public class Main {
         searchFrame.setVisible(true);
         ExelController controller = new ExelController();
         controller.test("src/main/resources/Data.xlsx");
+        List<String> firstRowValues = controller.getFirstRowValues("src/main/resources/Data.xlsx");
+
+        for (String value : firstRowValues) {
+            System.out.println(value);
+        }
+        System.out.println(controller.getcell(1,1,"src/main/resources/Data.xlsx"));
     }
 }
